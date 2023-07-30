0077b34d11b7874fe4055bccb6a71c212500a263
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-07-30 04:47:40 +0000

    Update jedis from 3.0.0 to 3.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val sparkStreamingKafkaVersion = "1.6.3"
   val sparkVersion = "2.4.0"
   val typesafeConfigVersion = "1.3.4"
-  val jedisVersion = "3.0.0"
+  val jedisVersion = "3.0.1"
   val hbaseVersion = "2.1.3"
   val hadoopVersion = "2.7.1"
   val prometheusVersion = "0.6.0"
