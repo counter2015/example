faef6207839fe73473cf681a34a838852c89bb2d
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-07-30 04:52:03 +0000

    Revert commit(s) 25fb5eb

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val kafkaVersion = "2.0.1"
   val sparkStreamingKafkaVersion = "1.6.3"
   val sparkVersion = "2.4.0"
-  val typesafeConfigVersion = "1.4.2"
+  val typesafeConfigVersion = "1.3.4"
   val jedisVersion = "3.0.0"
   val hbaseVersion = "2.1.3"
   val hadoopVersion = "2.7.1"
